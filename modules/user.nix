c183c8edc3b8b3f3c9bc6aef8f84dc28311bbc62
allank <allan.kent@gmail.com>
2025-05-25 13:45:26 +0200

    catppuccin all the things

--- modules/user.nix
+++ modules/user.nix
@@ -11,10 +11,31 @@
   programs.home-manager.enable = true;
 
   # CLI tools
-  programs.bat.enable = true;
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "Catppuccin-mocha";
+    };
+  };
   programs.eza.enable = true;
   programs.ripgrep.enable = true;
-  programs.fzf.enable = true;
+  programs.fzf = {
+    enable = true;
+    colors = {
+      "bg+" = "#313244";
+      "bg" = "#1e1e2e";
+      "spinner" = "#f5e0dc";
+      "hl" = "#f38ba8";
+      "fg" = "#cdd6f4";
+      "header" = "#f38ba8";
+      "info" = "#cba6f7";
+      "pointer" = "#f5e0dc";
+      "marker" = "#f5e0dc";
+      "fg+" = "#cdd6f4";
+      "prompt" = "#cba6f7";
+      "hl+" = "#f38ba8";
+    };
+  };
   programs.jq.enable = true;
   programs.neovim.enable = true;
 
